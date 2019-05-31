64f0340ae137c3bc53838c2d7852d6920413cc8d
yajie328 <996022662@qq.com>
2019-05-31 18:47:04 +0800

    lianxi2

--- tempCodeRunnerFile.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-fs.readFile("./promise/age.txt",()=>{
-    setTimeout(()=>{
-        console.log('timeout');
-    },0);
-    setImmediate(()=>{
-        console.log('immediate');
-    })
-})
